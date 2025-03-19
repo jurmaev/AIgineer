e89cf3e5a5665d7c6c87c021a162488d7e52aeca
jurmaev <smartemii03@gmail.com>
2025-03-19 20:40:01 +0500

    Добавить иконки

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -12,41 +12,43 @@ export function MainPage() {
   return (
     <>
       <Container>
-        <Stack gap={3} alignItems='center' marginBottom='40px'>
-          <Typography variant='gradient'>
-            Ваш верный помощник в создании уникальных поздравлений
-          </Typography>
-          <Typography variant='body1'>
-            Теперь не придется тратить кучу времени на придумывание
-            поздравлений, ведь ИИ сделает это вместо вас буквально за пару
-            кликов
-          </Typography>
-          <Button variant='contained' onClick={handleClick}>
-            Сгенерировать поздравление
-          </Button>
-        </Stack>
-        <Box display='flex' alignItems='center' flexDirection='column'>
-          <Typography variant='gradient' marginBottom='40px'>
-            Почему выбирают наш генератор?
-          </Typography>
-          <Stack gap={1}>
-            <Stack direction='row' gap='12px' alignItems='center'>
-              <LikeIcon />
-              <Typography variant='subtitle1'>Быстро и удобно</Typography>
-            </Stack>
-            <Stack direction='row' gap='12px' alignItems='center'>
-              <LightBulbIcon />
-              <Typography variant='subtitle1'>
-                Уникальные поздравления
-              </Typography>
-            </Stack>
-            <Stack direction='row' gap='12px' alignItems='center'>
-              <FreeIcon />
-              <Typography variant='subtitle1'>
-                Бесплатно и без регистрации
-              </Typography>
-            </Stack>
+        <Box margin='0 auto' paddingY={3} maxWidth='768px'>
+          <Stack gap={3} alignItems='center' marginBottom='40px'>
+            <Typography variant='gradient'>
+              Ваш верный помощник в создании уникальных поздравлений
+            </Typography>
+            <Typography variant='body1'>
+              Теперь не придется тратить кучу времени на придумывание
+              поздравлений, ведь ИИ сделает это вместо вас буквально за пару
+              кликов
+            </Typography>
+            <Button variant='contained' onClick={handleClick}>
+              Сгенерировать поздравление
+            </Button>
           </Stack>
+          <Box display='flex' alignItems='center' flexDirection='column'>
+            <Typography variant='gradient' marginBottom='40px'>
+              Почему выбирают наш генератор?
+            </Typography>
+            <Stack gap={1}>
+              <Stack direction='row' gap='12px' alignItems='center'>
+                <LikeIcon />
+                <Typography variant='subtitle1'>Быстро и удобно</Typography>
+              </Stack>
+              <Stack direction='row' gap='12px' alignItems='center'>
+                <LightBulbIcon />
+                <Typography variant='subtitle1'>
+                  Уникальные поздравления
+                </Typography>
+              </Stack>
+              <Stack direction='row' gap='12px' alignItems='center'>
+                <FreeIcon />
+                <Typography variant='subtitle1'>
+                  Бесплатно и без регистрации
+                </Typography>
+              </Stack>
+            </Stack>
+          </Box>
         </Box>
       </Container>
     </>
